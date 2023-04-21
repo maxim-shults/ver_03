ce29957afc26fa003a97d4b645eeb1f97fcd270b
maxim-shults <124172819+maxim-shults@users.noreply.github.com>
2023-04-21 10:03:30 +0300

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,26 +1,8 @@
-# import os
-# from Utils import *
-#
-# def clean_score_file():
-#     if os.path.exists("Scores.txt"):
-#         os.remove("Scores.txt")
-#
-# def add_score(difficulty):
-#     new_score = int(difficulty) * 3 + 5
-#     try:
-#         with open("Scores.txt", "r") as file:
-#             current_score = int(file.read())
-#     except FileNotFoundError:
-#         current_score = 0
-#
-#     total_score = current_score + new_score
-#     with open("Scores.txt", "w") as file:
-#         file.write(str(total_score))
-#import os
+
 from os.path import exists
 from Utils import *
 def add_score(difficulty):
-    #אם לא ניצח אין קובץ
+    
     POINTS_OF_WINNING = int(difficulty) * 3 + 5
     contents = None
     if exists(SCORES_FILE_NAME):
